89cc9ad1c5d1350d3d639f06e91e5fb07f5fceaf
Nithin PS <47279496+Nithinps021@users.noreply.github.com>
2023-09-09 10:57:52 +0000

    added removed old service

--- short-url-service/src/index.ts
+++ short-url-service/src/index.ts
@@ -10,7 +10,7 @@
 
 export interface Env {
 	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
+	MY_KV_NAMESPACE: "fce7ed1dff654189b040368f60632d2f";
 	//
 	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
 	// MY_DURABLE_OBJECT: DurableObjectNamespace;
@@ -24,9 +24,47 @@ export interface Env {
 	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
 	// MY_QUEUE: Queue;
 }
+import {Md5} from 'ts-md5';
 
+interface RequestData{
+	url: string,
+	time: string
+}
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		if(request.method == "POST"){
+			
+			//hashing the url
+			const requestData :RequestData = await request.json();
+			const url = requestData.url;
+			const hashString = md5hasing(url)
+
+			//storing in kv
+		
+			//return
+			return new Response(JSON.stringify({"hashedString": hashString}), {
+				headers:{
+					"content-type": "application/json;charset=UTF-8",
+				}
+			})
+		}
 		return new Response('Hello World!');
 	},
 };
+
+function md5hasing(url : string): string{
+	const timeInMillis = new Date().getTime()
+	const randomNumber = getRandomInt(100000000000,1000000000000);
+	const finalString = url+timeInMillis.toString()+randomNumber.toString()
+	const hashString = Md5.hashStr(finalString)
+	return hashString.slice(0,7);
+}
+
+function getRandomInt(min: number, max: number): number {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function storeInKV(hash: string){
+
+}
+  
